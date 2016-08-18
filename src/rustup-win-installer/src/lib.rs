a290f69cea84d75f97cab87fb11bf55a8959c724
Patrick Reisert <kpreisert@gmail.com>
2016-08-18 13:15:03 +0200

    Improve error handling

--- src/rustup-win-installer/src/lib.rs
+++ src/rustup-win-installer/src/lib.rs
@@ -5,7 +5,7 @@ extern crate rustup;
 
 use std::ffi::CString;
 use std::path::PathBuf;
-use ::winapi::{HRESULT, PCSTR, UINT, LPCWSTR, LPWSTR, LPVOID};
+use ::winapi::{HRESULT, PCSTR, UINT, LPCWSTR, LPWSTR, LPVOID, ERROR_INSTALL_FAILURE, ERROR_SUCCESS};
 
 pub type MSIHANDLE = u32;
 
@@ -13,76 +13,105 @@ pub const LOGMSG_TRACEONLY: i32 = 0;
 pub const LOGMSG_VERBOSE: i32 = 1;
 pub const LOGMSG_STANDARD: i32 = 2;
 
+type Result<T> = ::std::result::Result<T, ()>;
+
 // TODO: share this with self_update.rs
 static TOOLS: &'static [&'static str]
     = &["rustc", "rustdoc", "cargo", "rust-lldb", "rust-gdb", "rls"];
 
 #[no_mangle]
-/// This is run as an `immediate` action early in the install sequence
-pub unsafe extern "system" fn RustupSetInstallLocation(hInstall: MSIHANDLE) -> UINT {
-    // TODO: error handling (get rid of unwrap)
-    let name = CString::new("RustupSetInstallLocation").unwrap();
-    let hr = WcaInitialize(hInstall, name.as_ptr());
-    //let path = ::rustup::utils::cargo_home().unwrap();
-    let path = PathBuf::from(::std::env::var_os("USERPROFILE").unwrap()).join(".rustup-test");
-    set_property("RustupInstallLocation", path.to_str().unwrap());
-    WcaFinalize(hr)
+/// This is run as an `immediate` action early in the install sequence.
+/// It should only collect information and not modify the system in any way.
+pub unsafe extern "system" fn RustupPrepare(hInstall: MSIHANDLE) -> UINT {
+    do_custom_action(hInstall, "RustupPrepare", rustup_prepare)
 }
 
 #[no_mangle]
 /// This is be run as a `deferred` action after `InstallFiles` on install and upgrade
 pub unsafe extern "system" fn RustupInstall(hInstall: MSIHANDLE) -> UINT {
-    let name = CString::new("RustupInstall").unwrap();
-    let hr = WcaInitialize(hInstall, name.as_ptr());
+    do_custom_action(hInstall, "RustupInstall", rustup_install)
+}
+
+#[no_mangle]
+/// This is be run as a `deferred` action after `RemoveFiles` on uninstall (not on upgrade!)
+pub unsafe extern "system" fn RustupUninstall(hInstall: MSIHANDLE) -> UINT {
+    do_custom_action(hInstall, "RustupUninstall", rustup_uninstall)
+}
+
+fn rustup_prepare() -> Result<()> {
+    //let path = ::rustup::utils::cargo_home()
+    let path = try!(::std::env::var_os("USERPROFILE").map(|v| PathBuf::from(v).join(".rustup-test")).ok_or(()));
+    let path_str = try!(path.to_str().ok_or(()));
+    set_property("RustupInstallLocation", path_str)
+}
+
+fn rustup_install() -> Result<()> {
     // For deferred custom actions, all data must be passed through the `CustomActionData` property
-    let custom_action_data = get_property("CustomActionData");
+    let custom_action_data = try!(get_property("CustomActionData"));
     // TODO: use rustup_utils::cargo_home() or pass through CustomActionData
-    let path = PathBuf::from(::std::env::var_os("USERPROFILE").unwrap()).join(".rustup-test");
+    let path = try!(::std::env::var_os("USERPROFILE").map(|v| PathBuf::from(v).join(".rustup-test")).ok_or(()));
     let bin_path = path.join("bin");
     let rustup_path = bin_path.join("rustup.exe");
     let exe_installed = rustup_path.exists();
     log(&format!("Hello World from RustupInstall, confirming that rustup.exe has been installed: {}! CustomActionData: {}", exe_installed, custom_action_data));
     for tool in TOOLS {
         let ref tool_path = bin_path.join(&format!("{}.exe", tool));
-        ::rustup::utils::hardlink_file(&rustup_path, tool_path);
+        try!(::rustup::utils::hardlink_file(&rustup_path, tool_path).map_err(|_| ()))
     }
-    // TODO: install default toolchain and report progress to UI
-    WcaFinalize(hr)
+    // TODO: Install default toolchain and report progress to UI, but do not return Err
+    //       when toolchain installation fails, otherwise MSI will try to rollback the
+    //       whole installation which will leave hard links in place.
+    //       Maybe show a warning instead.
+    Err(())
 }
 
-#[no_mangle]
-/// This is be run as a `deferred` action after `RemoveFiles` on uninstall (not on upgrade!)
-pub unsafe extern "system" fn RustupUninstall(hInstall: MSIHANDLE) -> UINT {
-    let name = CString::new("RustupUninstall").unwrap();
-    let hr = WcaInitialize(hInstall, name.as_ptr());
+fn rustup_uninstall() -> Result<()> {
     // For deferred custom actions, all data must be passed through the `CustomActionData` property
-    let custom_action_data = get_property("CustomActionData");
+    let custom_action_data = try!(get_property("CustomActionData"));
     // TODO: use rustup_utils::cargo_home() or pass through CustomActionData
-    let path = PathBuf::from(::std::env::var_os("USERPROFILE").unwrap()).join(".rustup-test");
+    let path = try!(::std::env::var_os("USERPROFILE").map(|v| PathBuf::from(v).join(".rustup-test")).ok_or(()));
     let exe_deleted = !path.join("bin").join("rustup.exe").exists();
     log(&format!("Hello World from RustupUninstall, confirming that rustup.exe has been deleted: {}! CustomActionData: {}", exe_deleted, custom_action_data));
     // TODO: Remove .cargo and .rustup
-    ::rustup::utils::remove_dir("rustup-test", &path, &|_| {});
-    WcaFinalize(hr)
+    ::rustup::utils::remove_dir("rustup-test", &path, &|_| {}).map_err(|_| ())
 }
 
-// wrapper for WcaGetProperty (TODO: error handling)
-fn get_property(name: &str) -> String {
+unsafe fn do_custom_action(hInstall: MSIHANDLE, name: &str, action: fn() -> Result<()>) -> UINT {
+    let cname = CString::new(name).unwrap();
+    let init_success = succeeded(WcaInitialize(hInstall, cname.as_ptr()));
+     
+    // See https://msdn.microsoft.com/en-us/library/windows/desktop/aa368072(v=vs.85).aspx for error codes
+    if let Ok(_) = init_success.and_then(|_| action()) {
+        WcaFinalize(ERROR_SUCCESS as i32)
+    } else {
+        WcaFinalize(ERROR_INSTALL_FAILURE as i32)
+    }
+}
+
+fn succeeded(hr: HRESULT) -> Result<()> {
+    if hr >= 0 {
+        Ok(())
+    } else {
+        Err(())
+    }
+}
+
+// wrapper for WcaGetProperty
+fn get_property(name: &str) -> Result<String> {
     let encoded_name = to_wide_chars(name);
     let mut result_ptr = std::ptr::null_mut();
-    unsafe { WcaGetProperty(encoded_name.as_ptr(), &mut result_ptr) };
+    try!(succeeded(unsafe { WcaGetProperty(encoded_name.as_ptr(), &mut result_ptr) }));
     let result = from_wide_ptr(result_ptr);
     unsafe { StrFree(result_ptr as LPVOID) };
-    result
+    Ok(result)
 }
 
 // wrapper for WcaSetProperty
-fn set_property(name: &str, value: &str) -> HRESULT {
+fn set_property(name: &str, value: &str) -> Result<()> {
     let encoded_name = to_wide_chars(name);
     let encoded_value = to_wide_chars(value);
-    unsafe { WcaSetProperty(encoded_name.as_ptr(), encoded_value.as_ptr()) }
+    succeeded(unsafe { WcaSetProperty(encoded_name.as_ptr(), encoded_value.as_ptr()) })
 }
-
 
 fn log(message: &str) {
     let msg = CString::new(message).unwrap();
